4dc618950244045ec1870b750b343e9a83258d83
ngopashetty@yahoo.com <ngopashetty@yahoo.com>
2020-02-08 17:23:03 +0530

    .

--- WebApp/src/main/java/com/denext/SampleBean.java
+++ WebApp/src/main/java/com/denext/SampleBean.java
@@ -5,5 +5,23 @@ public class SampleBean {
 	String name;
 	int account;
 	double balance;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAccount() {
+		return account;
+	}
+	public void setAccount(int account) {
+		this.account = account;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
 
 }

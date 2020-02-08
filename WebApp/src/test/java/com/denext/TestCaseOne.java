836466c36eab08f50b83a2e637a0bc3e1915473f
ngopashetty@yahoo.com <ngopashetty@yahoo.com>
2020-02-08 17:28:26 +0530

    tescase

---
+++ WebApp/src/test/java/com/denext/TestCaseOne.java
@@ -0,0 +1,13 @@
+package com.denext;
+
+import junit.framework.TestCase;
+
+public class TestCaseOne extends TestCase {
+	
+	public void amhere()
+	{
+		System.out.println("Hell am here for you!");
+		System.out.println("This is a sample test case!");
+	}
+
+}
